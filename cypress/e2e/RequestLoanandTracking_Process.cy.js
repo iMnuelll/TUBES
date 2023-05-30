13b08e9e2a2f36e8b6af6babf73ee88d881787a6
IMANUEL UNEPUTTY <imanueluneputty121103@gmail.com>
2023-05-30 18:35:37 +0700

    pemai

--- cypress/e2e/RequestLoanandTracking_Process.cy.js
+++ cypress/e2e/RequestLoanandTracking_Process.cy.js
@@ -1,12 +1,12 @@
 describe("Login Parabank", () => {
   it("Visit the website", () => {
-    cy.visit("https://parabank.parasoft.com/parabank/index.htm");
+    cy.visit('https://parabank.parasoft.com/parabank/index.htm');
     cy.get(':nth-child(2) > .input').type('john')
     cy.get(':nth-child(4) > .input').type('demo')
     cy.get(':nth-child(5) > .button').click()
     cy.get('#leftPanel > ul > :nth-child(7) > a').click()
-    cy.get('#amount').type(500)
-    cy.get('#downPayment').type(250)
+    cy.get('#amount').type('1000')
+    cy.get('#downPayment').type('500')
     cy.get('#fromAccountId').select('13344')
     cy.get('[colspan="2"] > .button').click()
     cy.get('#newAccountId').click()
